b52bbe84a04bd7b38987ea0012d78f74f9e1c3cf
Steve Garon <steve.garon@cyber.gc.ca>
2021-02-12 15:05:23 -0500

    Fix service and added heuristic

--- whitelist-service.py
+++ whitelist-service.py
@@ -1,19 +1,11 @@
-import base64
-import datetime
-import json
+
 import os
-import re
-import tempfile
-import sys
-import hashlib
 import redis
 
-from tempfile import mkstemp
-from urllib.parse import urlparse
-
-from assemblyline.odm import IP_ONLY_REGEX
 from assemblyline_v4_service.common.base import ServiceBase
-from assemblyline_v4_service.common.result import Result, ResultSection, BODY_FORMAT
+from assemblyline_v4_service.common.result import Result, ResultSection, Heuristic
+
+REDIS_SERVER = os.environ.get('REDIS_SERVER', "whitelist-redisdb")
 
 
 class Whitelist(ServiceBase):
@@ -25,27 +17,13 @@ class Whitelist(ServiceBase):
             f"start() from {self.service_attributes.name} service called")
 
     def execute(self, request):
-        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-
-        sha1 = hashlib.sha1()
-
-        with open(request.file_path, 'rb') as f:
-            while True:
-                data = f.read(BUF_SIZE)
-                if not data:
-                    break
-                sha1.update(data)
-
-        fileHash = sha1.hexdigest().upper()
-
-        r = redis.Redis(host='redisdb', port=6379)
-        hashInDB = r.sismember ("hashes",fileHash)
-
-        if hashInDB:
-            self.log.info("{0} with SHA1 {1} found in whitelist".format(request.file_name, fileHash))
-            request.drop()
-        else:
-            self.log.info("{0} with SHA1 {1} NOT found in whitelist".format(request.file_name, fileHash))
-
         result = Result()
+        file_hash = request.sha1.upper()
+        r = redis.Redis(host=REDIS_SERVER, port=6379)
+
+        if r.sismember("hashes", file_hash):
+            msg = "{0} with SHA1 {1} found in whitelist".format(request.file_name, file_hash)
+            result.add_section(ResultSection(msg, heuristic=Heuristic(1)))
+            request.drop()
+
         request.result = result
